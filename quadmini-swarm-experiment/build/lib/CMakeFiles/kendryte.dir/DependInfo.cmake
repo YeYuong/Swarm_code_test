472b18819acbbfc8c6382648cdfea2097dd72f1f
yyl <3040184495@qq.com>
2023-10-30 19:27:17 +0800

    初始化飞机底层代码 将位置接口改为加速度接口

---
+++ quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/DependInfo.cmake
@@ -0,0 +1,71 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/bsp/crt.S" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/bsp/crt.S.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/bsp/entry.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/bsp/entry.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/bsp/entry_user.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/bsp/entry_user.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/bsp/interrupt.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/bsp/interrupt.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/bsp/locks.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/bsp/locks.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/bsp/printf.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/bsp/printf.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/bsp/sleep.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/bsp/sleep.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/bsp/syscalls.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/bsp/syscalls.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/bsp/w25qxx.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/bsp/w25qxx.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/bsp/w25qxx_dma.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/bsp/w25qxx_dma.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/aes.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/aes.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/apu.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/apu.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/clint.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/clint.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/dmac.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/dmac.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/dvp.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/dvp.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/fft.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/fft.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/fpioa.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/fpioa.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/gpio.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/gpio.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/gpiohs.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/gpiohs.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/i2c.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/i2c.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/i2s.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/i2s.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/kpu.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/kpu.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/plic.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/plic.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/pwm.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/pwm.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/rtc.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/rtc.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/sha256.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/sha256.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/spi.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/spi.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/sysctl.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/sysctl.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/timer.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/timer.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/uart.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/uart.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/uarths.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/uarths.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/utils.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/utils.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/drivers/wdt.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/drivers/wdt.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/vl53l1x/VL53L1X_api.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/vl53l1x/VL53L1X_api.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/vl53l1x/VL53L1X_calibration.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/vl53l1x/VL53L1X_calibration.c.obj"
+  "/home/yyl/gitcode/quadmini-swarm-experiment/lib/vl53l1x/vl53l1_platform.c" "/home/yyl/gitcode/quadmini-swarm-experiment/build/lib/CMakeFiles/kendryte.dir/vl53l1x/vl53l1_platform.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CONFIG_LOG_COLORS"
+  "CONFIG_LOG_ENABLE"
+  "CONFIG_LOG_LEVEL=LOG_VERBOSE"
+  "DEBUG=1"
+  "LOG_KERNEL"
+  "LV_CONF_INCLUDE_SIMPLE"
+  "__riscv64"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib/bsp/include"
+  "../lib/drivers/include"
+  "../lib/utils/include"
+  "../lib/vl53l1x/include"
+  "../src/quadmini"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
